f681a8305239f540cc86002935d5b06ccabb91aa
kooyunhyung <kooyh108@naver.com>
2023-05-09 14:49:12 +0900

    main.js fix

--- front-end/src/comp/Main.js
+++ front-end/src/comp/Main.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import '../App.css';
 
 function Main() {
   const [image, setImage] = useState(null);
+  const [items, setItems] = useState([]);
   const [dragging, setDragging] = useState(false);
 
   const handleDrop = async (e) => {
@@ -13,8 +15,29 @@ function Main() {
     reader.onloadend = async () => {
       const base64data = reader.result;
       setImage(base64data);
+      try {
+        const formData = new FormData();
+        formData.append("filepath", dataURItoBlob(base64data), file.name);
+        const response = await axios.post('http://210.115.229.250:5000/process_image', formData);
+        console.log(response.data);
+        setItems(response.data);
+      } catch (error) {
+        console.error(error);
+      }
     };
     setDragging(false);
+  };
+  
+  // dataURItoBlob 함수 추가
+  const dataURItoBlob = (dataURI) => {
+    const byteString = atob(dataURI.split(',')[1]);
+    const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+    const ab = new ArrayBuffer(byteString.length);
+    const ia = new Uint8Array(ab);
+    for (let i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ab], { type: mimeString });
   };
   
   const handleDragOver = (e) => {
@@ -28,7 +51,8 @@ function Main() {
   };
 
   return (
-    <div className={dragging ? "dragging" : ""} onDrop={handleDrop} onDragOver={handleDragOver} onDragLeave={handleDragLeave}>
+    <>
+      <div className={dragging ? "dragging" : ""} onDrop={handleDrop} onDragOver={handleDragOver} onDragLeave={handleDragLeave}>
       <h1>Drag and Drop Image Search</h1>
       <div className="dropzone">
         <p>Drop an image here to search for similar images</p>
@@ -37,6 +61,18 @@ function Main() {
         {image && <img src={image} alt="Dropped Image" />}
       </div>
     </div>
+    <div>
+     <div>
+     {items.map((item,index) => (
+        <img key={index} src={`http://210.115.229.250:5000/static/img/${item.class}/${item.path}`
+      } alt={item.class} />
+      ))}
+
+     </div>
+
+   
+    </div>
+    </>
   );
 }
 
