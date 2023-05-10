eed31d889d625e0addf309fbb1f656edeb346be6
Kangmj46 <kmjae0406@naver.com>
2023-05-10 23:47:33 +0900

    main.js

--- front-end/src/comp/result.js
+++ front-end/src/comp/result.js
@@ -1,4 +1,14 @@
 import React, { useState } from 'react';
-import '../App.css';
+import '../Res.css';
+
+function Result(){
+    return (
+        <div className = "main-page">
+            <iframe className = "image">
+
+            </iframe>
+        </div>
+    )
+}
 
 export default Result;
